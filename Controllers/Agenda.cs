a5d8c9b0133060c9e9b7525ab3ba7b96bb96844c
AriffMedina <ariffmedina.contact@gmail.com>
2026-05-08 22:58:16 -0600

    Sección 'Agenda' agregada

---
+++ Controllers/Agenda.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalogo.Controllers
+{
+    public class Agenda : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
